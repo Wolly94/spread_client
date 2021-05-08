b764d7786bccdd15c547c9f85e6a61069cf46d20
Wolly <golliwogg@gmx.de>
2021-05-08 12:17:24 +0200

    add startproduction script

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,7 +1,14 @@
 const baseUrl = () => {
-    if (process.env.NODE_ENV === 'development') return 'http://localhost:8765/'
-    else if (process.env.NODE_ENV === 'production')
+    if (
+        process.env.NODE_ENV === 'production' ||
+        process.env['REACT_APP_NODE_ENV'] === 'production'
+    ) {
+        console.log('run production')
         return 'http://3.128.245.168:8765/'
+    } else if (process.env.NODE_ENV === 'development') {
+        console.log('run development')
+        return 'http://localhost:8765/'
+    }
     return 'http://localhost:8765/'
 }
 
