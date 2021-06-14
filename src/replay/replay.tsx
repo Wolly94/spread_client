b1f19939b22f9e885ad7111551a8ed873d8b3fca
Wolly <golliwogg@gmx.de>
2021-06-14 11:00:55 +0200

    added controlbar and buttons to replayView

--- src/replay/replay.tsx
+++ src/replay/replay.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core'
+import { Box, Button, Grid } from '@material-ui/core'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { ClientGameState } from 'spread_game/dist/messages/inGame/clientGameState'
 import SpreadReplay from 'spread_game/dist/messages/replay/replay'
@@ -6,6 +6,8 @@ import { SpreadGameImplementation } from 'spread_game/dist/spreadGame'
 import { playerFromData } from 'spread_game/dist/spreadGame/player'
 import ClientGameStateView from './clientGameState'
 import useInterval from './../hooks/useInterval'
+import { ControlBar } from './controlBar'
+import MyButton from '../components/MyButton'
 
 interface ReplayProps {
     replay: SpreadReplay
@@ -27,23 +29,29 @@ const Replay: React.FC<ReplayProps> = ({ replay, ...props }) => {
         )
     }, [replay])
 
+    const updateScreen = useCallback(() => {
+        if (spreadGameRef.current !== null) {
+            setClientGameState(spreadGameRef.current.toClientGameState())
+        }
+    }, [])
+
     useEffect(() => {
         resetGame()
     }, [replay, resetGame])
 
-    const callback = useCallback(() => {
+    const stepCallback = useCallback(() => {
         if (spreadGameRef.current !== null) {
             // TODO scale by watch speed factor!
             spreadGameRef.current.runReplay(
                 replay,
                 replay.gameSettings.updateFrequencyInMs,
             )
-            setClientGameState(spreadGameRef.current.toClientGameState())
+            updateScreen()
         }
-    }, [replay])
+    }, [replay, updateScreen])
 
     const [paused, start, stop] = useInterval(
-        callback,
+        stepCallback,
         replay.gameSettings.updateFrequencyInMs,
     )
 
@@ -62,16 +70,62 @@ const Replay: React.FC<ReplayProps> = ({ replay, ...props }) => {
 
     useEffect(() => {
         start()
-        const x = 10
     }, [start]) // since start never changes, this will only be executed on first render
 
     return (
-        <Box>
+        <Box paddingLeft={5} paddingRight={5} paddingTop={5}>
             {clientGameState !== null && (
-                <ClientGameStateView
-                    map={replay.map}
-                    state={clientGameState}
-                ></ClientGameStateView>
+                <Grid container spacing={1}>
+                    <Grid item xs={3}>
+                        <Grid container>
+                            <Grid item xs={6}>
+                                <Button
+                                    onClick={() => {
+                                        if (paused) start()
+                                        else stop()
+                                    }}
+                                >
+                                    {paused ? 'Run' : 'Pause'}
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button
+                                    disabled={!paused}
+                                    onClick={() => {
+                                        stepCallback()
+                                    }}
+                                >
+                                    Step
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ControlBar
+                                    maxLengthInMs={replay.lengthInMs}
+                                    timePassedInMs={
+                                        clientGameState.timePassedInMs
+                                    }
+                                    setTime={(newTimePassedInMs) => {
+                                        stop()
+                                        resetGame()
+                                        if (spreadGameRef.current !== null) {
+                                            spreadGameRef.current.runReplay(
+                                                replay,
+                                                newTimePassedInMs,
+                                            )
+                                        }
+                                        updateScreen()
+                                    }}
+                                ></ControlBar>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ClientGameStateView
+                            map={replay.map}
+                            state={clientGameState}
+                        ></ClientGameStateView>
+                    </Grid>
+                </Grid>
             )}
         </Box>
     )
